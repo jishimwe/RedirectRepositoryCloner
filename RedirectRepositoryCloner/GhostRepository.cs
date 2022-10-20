26817a6b0d2ab05dc5556fde9303a864e9279fb0
jishimwe <jeanpaulishimwe@gmail.com>
2022-10-20 14:38:05 +0200

    repository cloner: first commit

---
+++ RedirectRepositoryCloner/GhostRepository.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace RedirectRepositoryCloner
+{
+	internal class GhostRepository
+	{
+		private Repository repository;
+		private string remoteDefaultName = "origin";
+		private string defaultBranchName = "master";
+		private string orphanBranchName = "ghost";
+		private string gitUser = "jishimwe";
+		private string gitMail = "jeanpaulishimwe@gmail.com";
+		private const string gitTokenPath = @"C:\Users\test\Documents\TFEToken";
+
+
+
+		public GhostRepository(string repoURL, string destPath, bool rdr = false)
+		{
+			this.repository = CloneBranchRepository(repoURL, destPath, rdr);
+		}
+
+		// Obsolete: keep just in case
+		public void GhostRepositoryDefault(string repoURL, string destPath)
+		{
+			repository = CloneBranchRepository(repoURL, destPath);
+			//repository = CloneEmptyRepository(repoURL, destPath);
+			Console.WriteLine("Repository cloned");
+			CheckoutFile(@"app/src/main/AndroidManifest.xml");
+			Console.WriteLine(@"app/src/main/AndroidManifest.xml checked out?");
+			CommitToRepository(@"C:\Users\test\Documents\GhostRepo\PlayMusic\app\src\main\AndroidManifest.xml", @"app/src/main/AndroidManifest.xml");
+			PushFile();
+			Console.WriteLine(@"app/src/main/AndroidManifest.xml pushed out?");
+			PrintGitStatus();
+		}
+
+		private string GetToken(string filepath = gitTokenPath)
+		{
+			if (filepath == null || !File.Exists(filepath))
+				return "";
+			string s = File.ReadAllText(filepath);
+			return s;
+		}
+
+		// Not really cloning a ghost branch -> removed ghost from the name
+		private Repository CloneBranchRepository(string repoURL, string destPath, bool rdr = false)
+		{
+			// If the directory already exist, we reset it. Deleting it's content careful which path you give
+			if (Directory.Exists(destPath))
+			{
+				Directory.Delete(destPath, true);
+				Directory.CreateDirectory(destPath);
+			}
+
+			var co = new CloneOptions
+			{
+				BranchName = defaultBranchName,
+				CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = gitUser, Password = GetToken() }
+			};
+			if (!rdr)
+				co.Checkout = false;
+			Repository.Clone(repoURL, destPath, co);
+
+			return new Repository(destPath);
+		}
+
+		// Obsolete: keep for legacy's sake
+		private Repository CloneEmptyRepository(string repoURL, string destPath)
+		{
+			string rootedPath = Repository.Init(destPath);
+			string logMessage = "fetch test";
+
+			Repository repo = new Repository(destPath);
+			//Remote remote = repo.Network.Remotes[remoteDefaultName];
+			Remote remote = repo.Network.Remotes.Add(remoteDefaultName, repoURL);
+			var refSpec = remote.FetchRefSpecs.Select(x => x.Specification);
+			Commands.Fetch(repo, remote.Name, null, null, logMessage);
+			return repo;
+			//return null;
+		}
+
+		public bool CheckoutFile(string filepath)
+		{
+			List<string> ls = new List<string>();
+			ls.Add(filepath);
+			repository.CheckoutPaths("master", ls, new CheckoutOptions());
+			return true;
+		}
+
+		private Branch CheckoutBranch(string bracnhName)
+		{
+			var branch = repository.Branches[bracnhName];
+			if (branch == null) return null;
+			CheckoutOptions co = new CheckoutOptions();
+			return Commands.Checkout(repository, branch);
+		}
+
+		public Commit CommitToRepository(string localFP, string remoteFP)
+		{
+			using (StreamWriter w = File.AppendText(localFP))
+			{
+				w.Write("\n<!-- Added with visual studio -->");
+				w.Close();
+			}
+			repository.Index.Add(remoteFP);
+			repository.Index.Write();
+			var status = repository.RetrieveStatus(new StatusOptions());
+			//Commands.Remove(repository, "./", false);
+			RemoveFromStaging();
+			Commands.Stage(repository, remoteFP);
+			Console.WriteLine(remoteFP + " staged \n");
+			//RemoveFromStaging();
+			Signature author = new Signature(gitUser, gitMail, DateTime.Now);
+			Signature committer = author;
+			//CommitOptions commitOptions = new CommitOptions();
+			//CommitFilter commitFilter = new CommitFilter();
+			//commitOptions.AllowEmptyCommit = true;
+			Commit commit = repository.Commit("test commit from Visual Studio", author, committer);
+			return commit;
+		}
+
+		public Commit CommitToRepository(Dictionary<string, string> filepaths, string message)
+		{
+			RemoveFromStaging();
+			foreach (var filepath in filepaths)
+			{
+				repository.Index.Add(filepath.Value);
+				repository.Index.Write();
+				Commands.Stage(repository, filepath.Value);
+			}
+
+			Signature author = new Signature(gitUser, gitMail, DateTime.Now);
+			Signature committer = author;
+
+			Commit commit = repository.Commit(message, author, committer);
+			return commit;
+		}
+
+		public void PrintGitStatus()
+		{
+			Console.WriteLine(repository.Commits);
+			foreach (var item in repository.RetrieveStatus(new StatusOptions()))
+			{
+				Console.WriteLine(item.FilePath);
+			}
+		}
+
+		private void RemoveFromStaging()
+		{
+			foreach (var item in repository.RetrieveStatus(new StatusOptions()))
+			{
+				if ((item.State & FileStatus.ModifiedInIndex) != 0)
+					continue;
+				//Commands.Remove(repository, item.FilePath, true);
+				Commands.Unstage(repository, item.FilePath);
+				//repository.Index.Remove(item.FilePath);
+				Console.WriteLine(item.FilePath + "\t" + item.State + "\t removed from staging");
+			}
+			Console.WriteLine();
+		}
+
+		public void PushFile()
+		{
+			Remote remote = repository.Network.Remotes[remoteDefaultName];
+			var opt = new PushOptions
+			{
+				CredentialsProvider = (_url, _user, _cred) =>
+					new UsernamePasswordCredentials { Username = gitUser, Password = GetToken() }
+			};
+
+			repository.Network.Push(remote, @"refs/heads/master", opt);
+		}
+	}
+}
---
+++ RedirectRepositoryCloner/Program.cs
@@ -0,0 +1,114 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Runtime.CompilerServices;
+using LibGit2Sharp;
+using RedirectRepositoryCloner;
+using static RedirectRepositoryCloner.GhostRepository;
+using static System.Net.WebRequestMethods;
+using File = System.IO.File;
+
+namespace RedirectRepositoryCloner
+{
+	public class Program
+	{
+		private const string defaultRepoUrl = "https://github.com/jishimwe/PlayMusic.git";
+		private const string defaultDestPath = @"C:\Users\test\Documents\GhostRepo\PlayMusic";
+		private const string defaultFileRelativePath = @"app/src/main/AndroidManifest.xml";
+		private const string defaultFileRealPath = @"C:\Users\test\Documents\GhostRepo\PlayMusic\app\src\main\AndroidManifest.xml";
+
+		static void Main(string[] args)
+		{
+			//HashSet<string> localFiles = new HashSet<string>();
+			Dictionary<string, string> localFiles = new Dictionary<string, string>();
+
+			if (args.Length <= 2)
+			{
+				Console.WriteLine("No arguments. Launching tests instances");
+				DefaultExec();
+				return;
+			}
+
+			string repoUrl = "", destPath = "", redirUrl = "", redirPath = "", tokenPath = "";
+
+			for (int i = 0; i < args.Length; i += 2)
+			{
+				string s = args[i];
+				switch (s)
+				{
+					case "-s":
+						repoUrl = args[i + 1];
+						break;
+
+					case "-d":
+						destPath = args[i + 1];
+						break;
+
+					case "-r":
+						redirUrl = args[i + 1];
+						break;
+
+					case "-p":
+						redirPath = args[i + 1];
+						break;
+
+					case "-t":
+						tokenPath = args[i + 1];
+						break;
+
+					default:
+						Console.WriteLine(args[i] + " is not a valid argument");
+						PrintUsage();
+						Console.WriteLine("Terminating process");
+						break;
+				}
+			}
+
+			if (repoUrl == "" || destPath == "" || redirPath == "" || redirUrl == "" || tokenPath == "")
+			{
+				Console.WriteLine("Invalid arguments");
+				PrintUsage();
+				Console.WriteLine("Terminating process");
+				return;
+			}
+
+			GhostRepository ghostRepository = new GhostRepository(repoUrl, destPath);
+			GhostRepository redirRepository = new GhostRepository(redirUrl, redirPath, true);
+
+			Console.WriteLine(repoUrl + " cloned into the directory " + destPath);
+			Console.WriteLine(redirUrl + " cloned into the directory " + redirPath);
+		}
+
+		private static void PrintUsage()
+		{
+			Console.WriteLine("Launch the program in a terminal with the following arguments:" +
+				"\n -s <Real repository> : The url for the project repository" +
+				"\n -d <Destination path>: The path where to put the cloned project repository" +
+				"\n -r <Redir repository>: The url for the redir repository" +
+				"\n -p <Redir path>      : The path where to put the cloned redir repository" +
+				"\n -t <Path to token>   : The path to the file containing a git token");
+			Console.WriteLine();
+		}
+
+		private static void DefaultExec()
+		{
+
+			Console.WriteLine("Testing with arguments :");
+			Console.WriteLine("Repository: " + defaultRepoUrl);
+			Console.WriteLine("Destination Folder: " + defaultDestPath);
+
+			GhostRepository ghostRepository = new GhostRepository(defaultRepoUrl, defaultDestPath);
+			Console.WriteLine("Repository cloned");
+
+			ghostRepository.CheckoutFile(defaultFileRelativePath);
+			Console.WriteLine(defaultFileRelativePath + " checked out?");
+
+			ghostRepository.CommitToRepository(defaultFileRealPath, defaultFileRelativePath);
+			ghostRepository.PushFile();
+			Console.WriteLine(defaultFileRelativePath + " pushed out?");
+
+			ghostRepository.PrintGitStatus();
+		}
+	}
+}
